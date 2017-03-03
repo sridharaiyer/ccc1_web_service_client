a81577177cf4d01b38ba32f955639c811a84674a
Sridhar Iyer <iyer.a.sri@gmail.com>
2017-03-03 17:00:33 -0600

    writing and reading from external payload xml unzipped file works

--- ccc1_web_service_python_client.py
+++ ccc1_web_service_python_client.py
@@ -6,7 +6,14 @@ from collections import defaultdict
 import json
 import xmltodict
 import os
-import xml.etree.ElementTree as etree
+from lxml import etree as ET
+import base64
+import tempfile
+import gzip
+# from xml.etree import ElementTree as ET
+
+# from xml.dom.minidom import parse
+# import xml.dom.minidom
 
 putPendingWorkfileXML = ''
 
@@ -45,7 +52,31 @@ with zipfile.ZipFile('Fiddler_Captures/cwf_testcase29_EO1.saz', 'r') as zf:
         with zf.open(os.path.join(files['Workfile'][0]), 'r') as wf:
             s = str(wf.read())
             envelope = [re.search(r'<s:Envelope.*\/s:Envelope>', s).group()][0]
-            # print(envelope)
-            root = etree.fromstring(envelope)
-            for child in root:
-                print(child)
+
+            root = ET.fromstring(envelope)
+
+            encoded_gzipped_payload = None
+
+            for elem in root.getiterator():
+                if elem.tag == '{http://services.mycccportal.com/SOA/normalizedmessage/v1}Data':
+                    encoded_gzipped_payload = elem.text
+
+            decoded_base64 = base64.b64decode(encoded_gzipped_payload)
+            # with tempfile.TemporaryFile(mode='w+b') as temp:
+            #     temp.write(bytes(decoded_base64))
+            #     print(temp.read())
+            #     with gzip.open(temp, 'rb') as f:
+            #         file_content = f.read()
+            #         print(file_content)
+
+            with open('decoded_base64.txt.gz', 'wb') as f:
+                f.write(decoded_base64)
+
+            with gzip.open('decoded_base64.txt.gz', 'rb') as f:
+                file_content = f.read()
+                print(file_content)
+
+            # envelope = ET.tostring(root)
+
+            # soup = BeautifulSoup(envelope, 'xml')
+            # print(soup.find('ClaimReferenceID'))
